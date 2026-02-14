18db895339bf3e7f1a9b33146260d7e086ff81c1
vikrant318 <m.a.yraj4108@gmail.com>
2026-02-14 14:14:55 +0530

    change the base path for production build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Social-media-dummy-app/',
+  base: '/Social_media_dummy_web/',
   plugins: [react()],
   server: {
     port: 3000,
